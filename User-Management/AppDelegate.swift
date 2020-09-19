5c53f15fee399acd3e6c39cb7a64b9be54b19d38
Kedar27 <kedarjsukerkar@yahoo.co.in>
2020-09-19 14:44:50 +0530

    Add - Project files.

---
+++ User-Management/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  User-Management
+//
+//  Created by Kedar Sukerkar on 19/09/20.
+//  Copyright © 2020 Kedar Sukerkar. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    // MARK: - Properties
+    var window: UIWindow?
+    
+    
+    // MARK: - UIApplicationDelegate Lifecycle
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+    
+    
+    
+}
+
